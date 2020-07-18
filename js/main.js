e6e4aa75a181ad342eb89e6267779b9cc3b7e856
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-07-18 13:12:31 -0400

    adding opacity to nav bar when scrolling

--- js/main.js
+++ js/main.js
@@ -10,17 +10,37 @@ function initMap() {
     var marker = new google.maps.Marker({position: toronto, map: map});
   }
   
-$('#navbar a, .btn').on('click', function(event){
-    if (this.hash !== ''){
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#navbar a, .btn").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
         event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            // -100 for the nav bar spacing
+          scrollTop: $(hash).offset().top - 65
+        }, 800, function(){
+  
+        });
+      } // End if
+    });
+  });
 
-        const hash = this.hash;
-
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            }, 
-            800
-        );
-    };
-});
+//   Sticky Menu Background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }
+    else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+})
